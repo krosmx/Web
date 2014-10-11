963d92cfc246bd48cb7311025be004204a4cba2b
MDOR <madmzr@gmail.com>
2014-10-11 14:03:33 -0500

    Added careers information templates

--- proyectos/UAA/js/main.js
+++ proyectos/UAA/js/main.js
@@ -1,23 +1,25 @@
-(function (window){
-    var bindList = function ( name, jsonInfo ){ 
-        var items = [];
-        for(var counter = 0, allRegisters = jsonInfo.length ; counter < allRegisters; counter++ ){
-            items[counter] = '<p>' + jsonInfo[counter] + '</p>';
-        }
-        return ('<header class="title">' + name + '</header>' + items.join(' ') );
+(function (window,$){
+
+    var  privateInformation = {
+        addEvents:(function(){
+
+            $('#career li').on('click', function( e ){
+                e.preventDefault();
+                e.stopPropagation();
+                var career = escape( $( this ).data( 'name' ) );                    
+
+                $('#information-container').load('careers/' + career + '.html', function(){
+                    var sizeContainer = 0;
+                    sizeContainer = $('.wording-container').height() || 500;
+                    $('.menu').height(sizeContainer);                    
+                } );
+            });
+
+        })
     }
-    $( '.linkInfo' ).on( 'click' , function(){
-       var items = [],
-           urlLink = 'json/' + escape($(this).attr('data-rel')) + '.json';
-        $.ajax({
-            url :  urlLink, 
-            dataType: "json",
-            success: function( data){
-                items[0] = bindList( 'Objetivo', data.objetive );
-                
-                
-                $('.Info-fluid').append( items.join(' ') );
-            }                    
-        });
-    });
-})(window)
+    return{
+        init: function(){
+            privateInformation.addEvents();
+        }
+    }
+})(window, jQuery).init();
